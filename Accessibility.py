c1b8bcd8b5470b4560b6a3bfcc7f7ee3bb435775
Dwayne A B Galloway <131449287+Dwaynewaynez@users.noreply.github.com>
2025-10-30 05:46:22 +0000

    Accessibility
    Access

---
+++ Accessibility.py
@@ -0,0 +1,12 @@
+from enum import Enum
+
+class NeuroProfile(Enum):
+    STANDARD = "standard"
+    EASY_READ = "easy_read"
+    ADHD_VOICE = "ADHD_voice"
+    SPECTRUM = "spectrum"
+    VISUAL = "visual"
+
+class AccessibilitySuite:
+    # Insert the AccessibilitySuite class code from your source here
+    pass
